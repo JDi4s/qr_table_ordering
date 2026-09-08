373df3b365bf591ce8ffe9fc873b157b369c9bd2
Joao Dias <joao107dias@gmail.com>
2026-09-08 21:58:25 +0100

    Load nested categories in manager menu

--- app/controllers/staff/menu_controller.rb
+++ app/controllers/staff/menu_controller.rb
@@ -1,5 +1,5 @@
 class Staff::MenuController < Staff::BaseController
   def index
-    @categories = current_establishment.categories.includes(:menu_items).order(:name)
+    @categories = current_establishment.categories.includes(:menu_items, :children).order(:name)
   end
 end
