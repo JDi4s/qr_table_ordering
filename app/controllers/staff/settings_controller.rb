7672b371dff8182ff151ae1dc93d7803e9986aee
Joao Dias <joao107dias@gmail.com>
2026-09-10 14:54:02 +0100

    Allow managers to save establishment logo

--- app/controllers/staff/settings_controller.rb
+++ app/controllers/staff/settings_controller.rb
@@ -2,6 +2,8 @@ class Staff::SettingsController < Staff::BaseController
   def edit; end
 
   def update
+    current_establishment.update!(establishment_params) if current_user.manager? && params[:establishment].present?
+
     if params.dig(:user, :staff_sound_enabled).present?
       current_user.update!(staff_sound_enabled: params.dig(:user, :staff_sound_enabled) == '1')
     end
@@ -10,5 +12,11 @@ class Staff::SettingsController < Staff::BaseController
       current_user.update!(password: password, must_change_password: false)
     end
     redirect_to edit_staff_settings_path, notice: 'Preferências guardadas.', status: :see_other
+  end
+
+  private
+
+  def establishment_params
+    params.require(:establishment).permit(:logo)
   end
 end
